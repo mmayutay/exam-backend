d6077d851749155d6207728d0336983dca930288
Raymond Jay Yorong <raymondjay.yorong@student.passerellesnumeriques.org>
2022-08-08 11:55:39 +0800

    Removed Comment

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,11 +7,6 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return Category::all();
